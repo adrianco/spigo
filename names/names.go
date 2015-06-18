c21ad44ffc992aeb2fa269352e8bc9660697b412
Adrian Cockcroft <adrian.cockcroft@gmail.com>
2015-06-18 10:44:37 -0700

    Added container hierarchy to names
    Not in active use yet, but names can now generate and manipulate
    container and process levels

--- names/names.go
+++ names/names.go
@@ -11,14 +11,17 @@ type hier int
 
 // offsets into the dot separated name
 const (
-	arch     hier = iota // netflixoss - architecture or AWS account level
-	region               // us-east-1a - AWS region or equivalent
-	zone                 // zoneA      - AWS availability zone or equivalent
-	service              // cassTurtle - service type or application name
-	ami                  // priamCassandra - versioned package of code to implement service, like AMI
-	instance             // cassTurtle0 - specific instance of service, like EC2 instance
+	arch      hier = iota // netflixoss - architecture or AWS account level
+	region                // us-east-1a - AWS region or equivalent
+	zone                  // zoneA      - AWS availability zone or equivalent
+	service               // cassTurtle - service type or application name
+	ami                   // priamCassandra - versioned package of code to implement service, like AMI
+	instance              // cassTurtle0 - specific instance of service, like EC2 instance
+	container             // docker container
+	process               // process id within container
 )
 
+// Split out a component of a name
 func Splitter(name string, offset hier) string {
 	s := strings.Split(name, ".")
 	if len(s) > int(offset) {
@@ -28,22 +31,32 @@ func Splitter(name string, offset hier) string {
 	}
 }
 
+// Make a service name from components and an index
 func Make(a, r, z, s, p string, i int) string {
 	return fmt.Sprintf("%v.%v.%v.%v.%v.%v%v", a, r, z, s, p, s, i)
 }
 
+// Make a container name from components and an index
+func MakeContainer(a, r, z, s, p, c string, i int) string {
+	return fmt.Sprintf("%v.%v.%v.%v.%v.%v%v.%v.%v", a, r, z, s, p, s, i, c, i)
+}
+
+// Extract architecture from a name
 func Arch(name string) string {
 	return Splitter(name, arch)
 }
 
+// Extract region from a name
 func Region(name string) string {
 	return Splitter(name, region)
 }
 
+// Extract zone from a name
 func Zone(name string) string {
 	return Splitter(name, zone)
 }
 
+// Return the other two zones, given one
 func OtherZones(name string, znames [3]string) [2]string {
 	var nz [2]string
 	for i, z := range znames {
@@ -55,6 +68,7 @@ func OtherZones(name string, znames [3]string) [2]string {
 	return nz
 }
 
+// Return the other regions given one
 func OtherRegions(name string, rnames []string) []string {
 	var nr []string
 	regions := len(rnames)
@@ -68,22 +82,37 @@ func OtherRegions(name string, rnames []string) []string {
 	return nr
 }
 
+// Extract the region and zone together
 func RegionZone(name string) string {
 	return Splitter(name, region) + ". " + Splitter(name, zone)
 }
 
+// Extract the service from a name
 func Service(name string) string {
 	return Splitter(name, service)
 }
 
+// Extract the AMI from a name
 func AMI(name string) string {
 	return Splitter(name, ami)
 }
 
+// Extract the package (same as AMI) from a name
 func Package(name string) string {
 	return AMI(name)
 }
 
+// Extract the instance from a name
 func Instance(name string) string {
 	return Splitter(name, instance)
+}
+
+// Extract the container from a name
+func Container(name string) string {
+	return Splitter(name, container)
+}
+
+// Extract the process from a name
+func Process(name string) string {
+	return Splitter(name, process)
 }
