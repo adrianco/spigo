8d4280980b32f12af4635ed4f5dbbb3ec7157aa5
Adrian Cockcroft <adrian.cockcroft@gmail.com>
2015-10-09 01:07:46 -0700

    flows with annotations
    zipkin-like timestamps for server receive messages

--- flow/flow.go
+++ flow/flow.go
@@ -8,10 +8,12 @@ import (
 	"github.com/adrianco/spigo/gotocol"
 	"log"
 	"os"
+	"time"
 )
 
-// flowmap is a map of requests of stuff, the next level of stuff is a map of parents, then a map of spans, holding a summary
+// flowmap is a map of requests of stuff, the next level of stuff is a map of parents, then a map of spans, holding a map of annotations
 type flowmaptype map[gotocol.TraceContextType]interface{}
+type annotationtype map[string]string
 
 var flowmap flowmaptype
 
@@ -40,7 +42,7 @@ func Begin(ctx gotocol.Context) {
 }
 
 // Update a flow, creating it if we need to Begin a new flow
-func Update(ctx gotocol.Context, s string) {
+func Update(ctx gotocol.Context, name string) {
 	if !archaius.Conf.Collect {
 		return
 	}
@@ -50,8 +52,32 @@ func Update(ctx gotocol.Context, s string) {
 	if flowmap[ctx.Trace].(flowmaptype)[ctx.Parent] == nil {
 		flowmap[ctx.Trace].(flowmaptype)[ctx.Parent] = make(flowmaptype)
 	}
-	flowmap[ctx.Trace].(flowmaptype)[ctx.Parent].(flowmaptype)[ctx.Span] = ctx.String() + "-" + s
+	annotation := make(annotationtype)
+	annotation["sr"] = fmt.Sprintf("%d", time.Now().UnixNano())
+	annotation["host"] = name
+	annotation["ctx"] = ctx.String()
+	flowmap[ctx.Trace].(flowmaptype)[ctx.Parent].(flowmaptype)[ctx.Span] = annotation
 }
+
+// Annotate a flow
+func Annotate(ctx gotocol.Context, name string, now time.Time) annotationtype {
+	if !archaius.Conf.Collect {
+		return nil
+	}
+	if flowmap[ctx.Trace] == nil {
+		Begin(ctx)
+	}
+	if flowmap[ctx.Trace].(flowmaptype)[ctx.Parent] == nil {
+		flowmap[ctx.Trace].(flowmaptype)[ctx.Parent] = make(flowmaptype)
+	}
+	annotation := make(map[string]string)
+	annotation["sr"] = fmt.Sprintf("%d", now.UnixNano()) // service receive time
+	annotation["host"] = name
+	annotation["ctx"] = ctx.String()
+	flowmap[ctx.Trace].(flowmaptype)[ctx.Parent].(flowmaptype)[ctx.Span] = annotation
+	return annotation
+}
+
 
 // Terminate a flow, flushing output and freeing the request id for re-use
 func End(ctx gotocol.Context) {
@@ -90,6 +116,8 @@ func Walk(flow flowmaptype) {
 			fmt.Println(x)
 		case flowmaptype:
 			Walk(x)
+		case annotationtype:
+			fmt.Println(x)
 		}
 	}
 }
