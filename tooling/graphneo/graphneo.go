aab20e66baaec1a75ec44a4c5f6005359221fafc
Adrian Cockcroft <adrian.cockcroft@gmail.com>
2016-05-12 08:52:53 +0100

    Neo4j writes test working
    Thanks to @jexp for pointing out what I missed. Still have lots more to
    do but unblocked initial bug.

--- tooling/graphneo/graphneo.go
+++ tooling/graphneo/graphneo.go
@@ -35,10 +35,11 @@ func Setup(arch string) {
 // Write an entry to the database
 func Write(str string) {
 	//log.Println(str)
-	stmt, err := db.Prepare(fmt.Sprintf("CREATE %v\n", str))
+	stmt, err := db.Prepare(str)
 	if err != nil {
 		log.Fatal(err)
 	}
+	stmt.Exec("")
 	log.Println(stmt)
 	stmt.Close()
 }
@@ -52,7 +53,7 @@ func WriteNode(nameService string, t time.Time) {
 	fmt.Sscanf(nameService, "%s%s", &node, &pack) // space delimited
 	tstamp := t.Format(time.RFC3339Nano)
 	// node id should be unique and package indicates service type
-	Write(fmt.Sprintf("\t(%v_%v:Node {name:%q, package:%q, timestamp:%q, ip:%q, region:%q, zone:%q})\nRETURN %v_%v", archaius.Conf.Arch+ss, names.Instance(node), node, pack, tstamp, dhcp.Lookup(node), names.Region(node), names.Zone(node), archaius.Conf.Arch+ss, names.Instance(node)))
+	Write(fmt.Sprintf("CREATE (%v_%v:Node {name:%q, package:%q, timestamp:%q, ip:%q, region:%q, zone:%q})\nRETURN %v_%v", archaius.Conf.Arch+ss, names.Instance(node), node, pack, tstamp, dhcp.Lookup(node), names.Region(node), names.Zone(node), archaius.Conf.Arch+ss, names.Instance(node)))
 }
 
 // WriteDone records that a node has gone away normally
@@ -70,7 +71,7 @@ func WriteEdge(fromTo string, t time.Time) {
 	var source, target string
 	fmt.Sscanf(fromTo, "%s%s", &source, &target) // two space delimited names
 	tstamp := t.Format(time.RFC3339Nano)
-	Write(fmt.Sprintf("(%v_%v)-[:CONNECTION {timestamp:%q}]->(%v_%v)\n", archaius.Conf.Arch+ss, names.Instance(source), tstamp, archaius.Conf.Arch+ss, names.Instance(target)))
+	Write(fmt.Sprintf("CREATE (%v_%v)-[:CONNECTION {timestamp:%q}]->(%v_%v)\n", archaius.Conf.Arch+ss, names.Instance(source), tstamp, archaius.Conf.Arch+ss, names.Instance(target)))
 }
 
 // WriteForget writes the forgotten edge to a file given a space separated edge id, from and to node names
