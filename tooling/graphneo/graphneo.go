a6ee9a3f61344289dfb076df92869e9a78b0b123
Adrian Cockcroft <adrian.cockcroft@gmail.com>
2016-05-12 08:18:49 +0100

    First attempt at Neo4j interface
    runs but doesn’t leave anything in Neo4j and I can’t figure out why.

---
+++ tooling/graphneo/graphneo.go
@@ -0,0 +1,89 @@
+// Package graphneo saves architectures to neo4j
+package graphneo
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/adrianco/spigo/tooling/archaius"
+	"github.com/adrianco/spigo/tooling/dhcp"
+	"github.com/adrianco/spigo/tooling/names"
+	_ "gopkg.in/cq.v1"
+	"log"
+	"os"
+	"time"
+)
+
+// Enabled is set via command line flags to turn on neo logging
+var Enabled bool
+var db *sql.DB
+var ss string
+
+// Setup by opening the "arch".json file and writing a header, noting the generated architecture
+// type, version and args for the run
+func Setup(arch string) {
+	Enabled = true
+	if archaius.Conf.StopStep > 0 {
+		ss = fmt.Sprintf("%v", archaius.Conf.StopStep)
+	}
+	tmp, err := sql.Open("neo4j-cypher", "http://neo4j:"+os.Getenv("NEO4JPASSWORD")+"@localhost:7474")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db = tmp
+}
+
+// Write an entry to the database
+func Write(str string) {
+	//log.Println(str)
+	stmt, err := db.Prepare(fmt.Sprintf("CREATE %v\n", str))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(stmt)
+	stmt.Close()
+}
+
+// WriteNode writes the node to a file given a space separated name and service type
+func WriteNode(nameService string, t time.Time) {
+	if Enabled == false {
+		return
+	}
+	var node, pack string
+	fmt.Sscanf(nameService, "%s%s", &node, &pack) // space delimited
+	tstamp := t.Format(time.RFC3339Nano)
+	// node id should be unique and package indicates service type
+	Write(fmt.Sprintf("\t(%v_%v:Node {name:%q, package:%q, timestamp:%q, ip:%q, region:%q, zone:%q})\nRETURN %v_%v", archaius.Conf.Arch+ss, names.Instance(node), node, pack, tstamp, dhcp.Lookup(node), names.Region(node), names.Zone(node), archaius.Conf.Arch+ss, names.Instance(node)))
+}
+
+// WriteDone records that a node has gone away normally
+//func WriteDone(name string, t time.Time) {
+//	if Enabled == false {
+//		return
+//	}
+//}
+
+// WriteEdge writes the edge to a file given a space separated from and to node name
+func WriteEdge(fromTo string, t time.Time) {
+	if Enabled == false {
+		return
+	}
+	var source, target string
+	fmt.Sscanf(fromTo, "%s%s", &source, &target) // two space delimited names
+	tstamp := t.Format(time.RFC3339Nano)
+	Write(fmt.Sprintf("(%v_%v)-[:CONNECTION {timestamp:%q}]->(%v_%v)\n", archaius.Conf.Arch+ss, names.Instance(source), tstamp, archaius.Conf.Arch+ss, names.Instance(target)))
+}
+
+// WriteForget writes the forgotten edge to a file given a space separated edge id, from and to node names
+//func WriteForget(fromTo string, t time.Time) {
+//	if Enabled == false {
+//		return
+//	}
+//}
+
+// Close the database session
+func Close() {
+	if Enabled == false {
+		return
+	}
+	db.Close()
+}
---
+++ tooling/graphneo/graphneo_test.go
@@ -0,0 +1,26 @@
+// graphjson tests - just make sure the json conversions work
+package graphneo
+
+import (
+	"github.com/adrianco/spigo/tooling/archaius"
+	"github.com/adrianco/spigo/tooling/names"
+	"testing"
+	"time"
+)
+
+// reader parses graphjson
+func TestGraph(t *testing.T) {
+	testNeo := `
+	  (test_mysql00:Node {name:"test.us-east-1.zoneA..mysql00...mysql.store", package:"store", timestamp:"2016-04-17T13:40:05.938437713-07:00", ip:"54.198.0.1", region:"us-east-1", zone: "zoneA"}),
+          (test_mysql01:Node {name:"test.us-east-1.zoneA..mysql01...mysql.store", package:"store", timestamp:"2016-04-17T13:40:05.938513762-07:00", ip:"54.221.0.1", region:"us-east-1", zone: "zoneA"}),
+          (test_mysql00)-[:CONNECTION]->(test_mysql01)
+                `
+	archaius.Conf.Arch = "test"
+	Setup(archaius.Conf.Arch)
+	Write(testNeo)
+	dal0 := names.Make("test", "us-east-1", "ZoneA", "dal", "staash", 0)
+	WriteNode(dal0+" staash", time.Now())
+	WriteEdge(dal0+" test.us-east-1.zoneA..mysql00...mysql.store", time.Now())
+	WriteEdge(dal0+" test.us-east-1.zoneA..mysql01...mysql.store", time.Now())
+	Close()
+}
